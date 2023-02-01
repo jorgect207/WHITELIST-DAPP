db93467bb4856ddf3c4324fb693e28c0ea32fc3a
jorgect207 <49347492+jorgect207@users.noreply.github.com>
2023-01-31 20:18:21 -0500

    Delete whitelistdappfront directory

--- whitelistdappfront/src/Components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./Card.module.css";
-
-const Card = (props) => {
-  <div className={classes.card}>{props.children}</div>;
-};
-
-export default Card;
